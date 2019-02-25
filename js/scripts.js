44322dfaaf5bf843179419918f85dad37c1b08df
NyaunchoDavid <donchuchoh@gmail.com>
2019-02-25 07:28:57 +0300

    delete class send style in css

--- js/scripts.js
+++ js/scripts.js
@@ -5,13 +5,13 @@ $(function(){
     var queFive = parseInt($("input:radio[name=sour]:checked").val());
     var correct = (queThree  + queFour + queFive);
     var percentage = (correct / 3 * 100);
-    $("#send").text("You have scored: " + percentage + "%");
+    $(".send").text("You have scored: " + percentage + "%");
   if (percentage > 80) {
-    $("#see").text("You have passed hurrah!");
+    $(".see").text("You have passed hurrah!");
   } else if ((percentage > 50) && (percentage < 79)) {
-    $("#see").text("You tried!");
+    $(".see").text("You tried!");
   } else {
-    $("#see").text("You Have Failed!");
+    $(".see").text("You Have Failed!");
   }
     event.preventDefault();
   });
