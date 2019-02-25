e1e19a4e8bf35c6af9de1bf23a76379c18769ae8
NyaunchoDavid <donchuchoh@gmail.com>
2019-02-25 07:59:45 +0300

    add parenthises to variable correct

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,17 @@
 $(function(){
-  $("#week").submit(function(event){
+  $("form#week").submit(function(event){
     var queThree = parseInt($("input:radio[name=flavor]:checked").val());
     var queFour = parseInt($("input:radio[name=taste]:checked").val());
     var queFive = parseInt($("input:radio[name=sour]:checked").val());
     var correct = (queThree  + queFour + queFive);
-    var percentage = (correct / 3 * 100);
-    $(".send").text("You have scored: " + percentage + "%");
+    var percentage = ((correct / 3) * 100);
+    $("#send").text("You have scored: " + percentage + "%");
   if (percentage > 80) {
-    $(".see").text("You have passed hurrah!");
+    $("#see").text("You have passed hurrah!");
   } else if ((percentage > 50) && (percentage < 79)) {
-    $(".see").text("You tried!");
+    $("#see").text("You tried!");
   } else {
-    $(".see").text("You Have Failed!");
+    $("#see").text("You Have Failed!");
   }
     event.preventDefault();
   });
