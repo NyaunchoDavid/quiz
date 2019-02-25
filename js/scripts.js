cea3bf2a9677b6b72ced4a7492f16c4647b70d68
NyaunchoDavid <donchuchoh@gmail.com>
2019-02-25 07:22:06 +0300

    add div class form-group

--- js/scripts.js
+++ js/scripts.js
@@ -7,11 +7,11 @@ $(function(){
     var percentage = (correct / 3 * 100);
     $("#send").text("You have scored: " + percentage + "%");
   if (percentage > 80) {
-    $(".ans").text("You have passed hurrah!");
+    $("#see").text("You have passed hurrah!");
   } else if ((percentage > 50) && (percentage < 79)) {
-    $(".ans").text("You tried!");
+    $("#see").text("You tried!");
   } else {
-    $(".ans").text("You Have Failed!");
+    $("#see").text("You Have Failed!");
   }
     event.preventDefault();
   });
